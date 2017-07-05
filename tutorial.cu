ba856343c32e5d3fe887a052417f8e883260c89e
Matt Angus <m2angus@wise11.eng.uwaterloo.ca>
2017-07-05 13:59:57 -0400

    removed code for barebones start

--- tutorial.cu
+++ tutorial.cu
@@ -4,19 +4,12 @@
 #include "tensorflow/core/util/cuda_kernel_helper.h"
 
 template <typename dtype> __global__ void AddKernel(const dtype* a, const dtype* b, dtype* c, int N){
-	CUDA_1D_KERNEL_LOOP(index, N)
-	{
-		c[index] = a[index] + b[index];
-	}
+
 }
 
 template <typename dtype>
 void launchAddKernel(const dtype* a, const dtype* b, dtype* c, int N) {
 	const int kThreadsPerBlock = 1024;
-	
-	AddKernel<dtype><<<(N + kThreadsPerBlock - 1) / kThreadsPerBlock,
-					kThreadsPerBlock>>>(
-			a, b, c, N);
 
 	cudaError_t cudaerr = cudaDeviceSynchronize();
 	if (cudaerr != cudaSuccess)
@@ -24,14 +17,3 @@ void launchAddKernel(const dtype* a, const dtype* b, dtype* c, int N) {
 			cudaGetErrorString(cudaerr));
 }
 
-
-//forward declaration for all the types needed
-#define ADD_KERNEL_TYPE(type)							\
-	template void launchAddKernel<type>(				\
-		const type* a, const type* b, type* c, int N)	\
-
-ADD_KERNEL_TYPE(int);
-ADD_KERNEL_TYPE(float);
-ADD_KERNEL_TYPE(double);
-
-#undef ADD_KERNEL_TYPE
