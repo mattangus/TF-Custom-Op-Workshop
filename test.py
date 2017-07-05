bb73e43bac1a6b4c1d89f039b59a64a22d8a2c6c
Matt Angus <m2angus@wise11.eng.uwaterloo.ca>
2017-07-05 15:12:40 -0400

    added missmatch example

--- test.py
+++ test.py
@@ -15,9 +15,11 @@ shape = (1,10,10,3)
 
 a_data = np.random.random(shape)
 b_data = np.random.random(shape)
+#b_data = np.random.random((1,11,10,3))
 
 a = tf.constant(a_data, shape=shape, name="a")
 b = tf.constant(b_data, shape=shape, name="b")
+#b = tf.constant(b_data, shape=(1,11,10,3), name="b")
 
 c_cust = custom_add(a,b)
 
